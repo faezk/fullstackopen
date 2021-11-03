ce16e12ee5263f97c9767fca0134b6b6a86c744a
faeze kermani <kermani.faeze@gmail.com>
2021-11-03 11:23:10 +0200

    Commit part1-exercise 1.1 & 1.2

---
+++ part1/CourseInfo/1.2/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react'
+const Header=(props)=>{
+  return (
+    <>
+      <h1>{props.course}</h1>
+   </>
+  )
+}
+// const Content=(props)=>{
+//   return(
+//     <>
+//     <p>{props.part} {props.exercises}</p>
+//     </>
+//   )
+// }
+
+//exercise 2
+const Part=(props) =>{
+  return(
+    <>
+    <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+const Content =(props)=> {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercise1} />
+      <Part part={props.part2} exercises={props.exercise2} />
+      <Part part={props.part3} exercises={props.exercise3} />
+    </>
+  )
+}
+
+
+const Total=(props)=>{
+  return(
+    <>
+     <p>Number of exercises {props.totalNumber}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total=exercises1+exercises2+exercises3
+
+  return (
+    <div>
+      <Header course={course} />
+       <Content part1={part1}  exercise1={exercises1} part2={part2} exercise2={exercises2}
+        part3={part3} exercise3={exercises3}/>
+       <Total totalNumber={total} /> 
+    </div>
+  )
+}
+export default App
