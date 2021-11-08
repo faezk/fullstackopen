9c23af597c4777d9b207d85f61e5670b997926d3
faeze kermani <kermani.faeze@gmail.com>
2021-11-08 11:51:27 +0200

    Change exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,57 +7,48 @@ const Button = ({ onClick, text }) => (
     {text}
   </button>
 )
-const Statistic=({text,type})=>(
-    
+const Statisticline=({text,type})=>(    
     <div><b>{text}</b>: {type} </div>
 )
+
 
 const App = () => {
 
 const [good, setGood] = useState(0)
 const [neutral, setNeutral] = useState(0)
 const [bad, setBad] = useState(0)
+const [value, setValue] = useState(0)
+const [allClicks, setAll] = useState(0)
 const Isdisplay=((good!==0) || (neutral!==0) || (bad!==0)) ? true:false; 
-const all=good + neutral + bad;
-const avrage=(good, neutral,bad, all)=>{
-  if(all !==0){
-    return ((good*1) + (neutral*0) + (bad*-1) )/all 
-  }
-  else{
-    return 0;
-  }
-}
-const positive=(good, all)=>{
-  if(all !==0){
-    return (good*100 )/all 
-  }
-  else{
-    return 0;
-  }
-}
+
 return (
   <div>
     <h1>Feedback here</h1>
-    <Button onClick={() => setGood(good + 1)}
-      text='Good'
-    />
-        <Button onClick={() => setNeutral(neutral + 1)}
-      text='Neutral'
-    />
-        <Button onClick={() => setBad(bad + 1)}
-      text='bad'
-    />
-    
+      <Button onClick={() => {
+        setGood(good + 1) 
+        setValue(value + 1) 
+        setAll(allClicks + 1)}} text="Good" />
+
+     <Button onClick={() => {
+      setNeutral(neutral + 1) 
+      setValue(value + 0) 
+      setAll(allClicks + 1)}} text="Neutral" />
+
+     <Button onClick={() => {
+      setBad(bad + 1)
+      setValue(value - 1) 
+      setAll(allClicks + 1)}} text="Bad" />
+
       <h1>Statistics</h1>
        {Isdisplay
-        ? <div>
-        <Statistic text="Good" type={good}/>
-        <Statistic text="neutral" type={neutral}/>
-        <Statistic text="bad" type={bad}/>
-        <Statistic text="All" type={all}/>
-        <Statistic text="avrage" type={avrage(good, neutral, bad, all)}/>
-        <Statistic text="positive" type={positive(good, all)}/>
-       </div>
+        ?<div>        
+        <Statisticline text="Good" type={good}/>
+        <Statisticline text="neutral" type={neutral}/>
+        <Statisticline text="bad" type={bad}/>
+        <Statisticline text="All" type={allClicks}/>
+        <Statisticline text="Avrage" type={value/allClicks}/>
+        <Statisticline text="positive" type={(good*100 )/allClicks+'%'}/>
+        </div>
        :<h3>No feedback given</h3>}               
   </div>
 )
