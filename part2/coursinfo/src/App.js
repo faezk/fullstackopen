f405f9ee373fba50c4afd6b9d65e2ffd7ad88574
faeze kermani <kermani.faeze@gmail.com>
2021-11-11 17:43:28 +0200

    part2 courseinfo 2.2 & 2.3

--- part2/coursinfo/src/App.js
+++ part2/coursinfo/src/App.js
@@ -3,10 +3,23 @@ const Course=({ course }) => {
   return (
     <>
      <h1>{course.name}</h1>
-     {course.parts.map(x => <p key={x.id}>{x.name + x.exercises} </p>)}
+     {course.parts.map(x => <p key={x.id}>{x.name +" "+ x.exercises} </p>)}
+    <Total parts={course.parts}/>
    </>
   )
 }
+
+ const Total = ({ parts })=>{
+  //  console.log(parts);
+   return(
+     <>
+    <p>Total of <b>{parts.reduce((sum, part)=> sum + part.exercises, 0)}</b> exercises</p> 
+     </>
+   )
+ 
+}
+
+
 const App = () => {
   const course = {
     id: 1,
@@ -26,6 +39,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'redux',
+        exercises: 10,
+        id: 4
       }
     ]
   }
