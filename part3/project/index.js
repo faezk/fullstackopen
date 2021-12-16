be410488f53e8868939130d7de07aa44d4a4de17
faeze kermani <kermani.faeze@gmail.com>
2021-12-16 19:52:36 +0200

    Add Exercise 3.13 to 3.14

--- part3/project/index.js
+++ part3/project/index.js
@@ -1,34 +1,17 @@
-
+require('dotenv').config()
 const express = require('express')
 let morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
+
 
 
 app.use(cors())
 app.use(express.json())
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+
+
 
 //Define  post token
 morgan.token('post', (req, res) => JSON.stringify(req.body))
@@ -56,70 +39,71 @@ app.use(morgan(function (tokens, req, res) {
 
 
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
 
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
 
 
 app.get('/info',(request,response)=>{
-  //const person = persons.find(person => person)
-   const currentDate = new Date().toLocaleString();
+  const currentDate = new Date().toLocaleString();
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-   response.send(      
-    ` <p>Phonebook has info for ${persons.length} people</p>    
-      <p>${currentDate} (${timeZone})</p>`  
-   )
+
+  Person.find({})
+  .then(persons=>{
+    response.send(      
+      ` <p>Phonebook has info for ${persons.length} people</p>    
+        <p>${currentDate} (${timeZone})</p>`  
+     )
+  })
+   
 })
+
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-     response.status(404).end()
- 
-  }
+  })
 })
 
-function generateId() {
-  return Math.floor(Math.random() * 1000);
-}
+
 
 app.post('/api/persons', (request, response) => {
 
   const body = request.body
-
+ 
     if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'The name or number is missing' 
-    })
+    return response.status(400).json({ error: 'The name or number is missing' })
   }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
   
-  const personExist=persons.find(person => person.name === body.name)
-   if(personExist) {
-    return response.status(400).json({ 
-       error: 'name must be unique' 
-    })
-  }
+  
+  // const personExist=Person.find(person => person.name === body.name)
+  //  if(personExist) {
+  
+  //   return response.status(400).json({ 
+  //      error: 'name must be unique' 
+  //   })
+  // }
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
-  persons = persons.concat(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 
-  response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  const id = request.params.id
+  Person.findByIdAndRemove(id).then(result => {
+    response.status(204).end()
+  })
+ 
 })
 
 
@@ -129,8 +113,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-
-  const PORT =  process.env.PORT || 3001
-  app.listen(PORT, () => {
+const PORT = process.env.PORT
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
